953d9126f276b6fd180dc1bd8f00c5cc9cf2844b
Rahand Amin <87046788+Rahand-amin@users.noreply.github.com>
2021-10-15 02:34:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// Create a Set
+const letters = new Set();
+
+// Create Variables
+
+
+
+function myfunction() {
+
+    let a = document.getElementById("nusin");
+    letters.add(a.value);
+    let text = "";
+    letters.forEach(function (value) {
+        text += value + "<br>";
+    })
+
+    document.getElementById("demo").innerHTML = text;
+
+};
+
+function myFunction() {
+
+    let a = document.getElementById("nusin");
+    letters.delete(a.value);
+    let text = "";
+    letters.forEach(function (value) {
+        text += value + "<br>";
+    })
+
+    document.getElementById("demo").innerHTML = text;
+
+};
+
+
+// Add the Variables to the Set
+
+
+
+// Display set.size
